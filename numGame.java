7464cc6a2aa784983a8a41cd595225fe31752991
Malea Cesar <maleacesar@Maleas-MacBook-Air.local>
2024-11-05 10:39:44 -0600

    with sorting array

--- numGame.java
+++ numGame.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 class numGame{
 
     public int[] numberGame(int[] nums) {
+        //Priority Queue time complexity is nlog(n). 
         PriorityQueue<Integer> queue = new PriorityQueue<>();
         int arr[] = new int[nums.length];
 
@@ -20,6 +22,16 @@ class numGame{
             arr[index] = alice;
             index++;
         }   
+
+        //OR DO THE FOLLOWING
+        
+        /*Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 0; i<n-1;  i= i+2){
+            int tmp = arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]= tmp;
+        }*/
         
         return arr;
     }
